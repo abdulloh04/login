d2aef634bba92058ead537d0599d6934c8832991
Abdulloh Uyg'unov <0048242a@gmail.com>
2021-10-10 20:46:20 +0500

    tanlash qism tayyor

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ db = mysql.connector.connect(
     database = "login"
 )
 
+
 class Loginpage:
     def __init__(self):
         self.login = None
@@ -24,11 +25,30 @@ class Loginpage:
         Login   [2]
         
         [1/2]: """)
+        tanlash_option = ['1', '2']
+
+        while tanlash not in tanlash_option:
+            self.clear_and_invalid("Faqat 1 va 2 raqamni kiriting!!!")
+            tanlash = input("[1/2]: ")
+
+        if tanlash == tanlash_option[0]:
+            self.registr()
+        else:
+            self.login()
+
+    def registr(self):
+        print("Regist qism")
+
+    def login(self):
+        print("Login qism")
 
 
     def clear(self):
         os.system("clear")
 
+    def clear_and_invalid(self, string: str):
+        self.clear()
+        print(f"{string}")
 
 
 
